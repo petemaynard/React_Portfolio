afae19be731dcee6f70ff24e84fd6a415e667b24
petemaynard <xerxespete@gmail.com>
2024-03-19 23:28:30 -0500

    Updated resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,15 +1,42 @@
 export default function Resume() {
-  return (
-    <div>
-      <h2>My Resume</h2>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
-    </div>
-  );
+   return (
+      <div>
+         <h2>My Resume</h2>
+
+         <h4>SOFTWARE ENGINEER</h4>
+         <p>
+            A highly skilled Software Engineer with extensive experience in all stages of product development and application support within an enterprise-level environment. Proficient in multiple technologies, including UNIX/Linux, SQL Server, Teradata, and ETL programming with Ab Initio. Hold AWS Certified Cloud Practitioner certification.
+         </p>
+         <br></br>
+         <h4>Skills</h4>
+         <p>
+            UNIX/Linux (bash/ksh) | SQL Server | Teradata | ETL | JIRA | Agile | perl | Jenkins | uDeploy Autosys | MQ Messaging | ServiceNow | Workday | Confluence | Cronacle | C | COBOL | HTML
+         </p>
+         <br></br>
+         <h4>Work History</h4>
+         <p>
+            <b>Wells Fargo, June 2007 to November 2022</b>
+            <ul>
+               <li>Designed, deployed, and maintained Ab Initio-based enterprise-wide application used for marketing and legal teams to comply with millions of customers&#39; and non-customers&#39; legal and regulatory communication status. </li>
+               <li>CI/CD with Jenkins and UrbanCode Deploy (IBM uDeploy)</li>
+               <li>Extensive use of Teradata and SQL Server, including creation and updating of stored procedures and macros</li>
+               <li>Validated contents of EPSS databases matched Hogan mainframe data</li>
+               <li>Installed encryption key for SQL Server fields with Personally Identifiable Information, modifying 130+ code objects at once to use the new key</li>
+               <li>Resolved complex software, database, or data-related issues involving aborted production jobs while monitoring Autosys scheduler jobs on 24/7 schedule, including nightly and monthly batch processing, to meet SLAs, or delegated to off-shore  developers, using JIRA ticketing</li>
+               <li>Applied Agile work management through entire SDLC, including daily stand-ups, using JIRA project management.</li>
+               <li>Coordinated with Infrastructure and Support teams during planned outages/patching using ServiceNow</li>
+               <li>Created and reviewed test plans for developers&#39; changes</li>
+               <li>Designed and executed semi-yearly BCP contingency testing to failover servers</li>
+               <li>Entrusted with roles allowing access to sensitive customer data</li>
+               <br></br>
+
+               <h4>Software Engineer</h4>
+               <b>Navitaire (formerly PRA Solutions), 2000 to 2006</b>
+               <li> Analyzed, designed, and developed or modified new and existing C and Microfocus COBOL programs throughout entire SDLC in a Solaris UNIX environment for some of the world’s largest airlines. Included using various debuggers such as lint, Workshop (for C programs,) and Animator (for COBOL.)</li>
+               <li> Designed, executed, and documented comprehensive testing plans for use in unit and integration testing, including constructing SQL queries to extract production data to be used for lower environment testing. Coordinated with QA team to design and  to document QA testing process and its expected results and consulted with production team on migration effects and timing.</li>
+               <li> Developed C shell scripts that executed batch programs, performed error checking, automated ftp, etc.</li>
+            </ul>
+         </p>
+      </div>
+   );
 }
